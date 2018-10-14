711c2365c7a378c9b51a19c5e96469dd84cad341
Joey Kathan <kathan.joey@gmail.com>
2018-10-14 11:26:47 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,21 +1,59 @@
  const express = require('express');
  const app = express();
 
- const PORT = process.env.PORT || 3000;
-
 const cors = require('cors');
-//const {CLIENT_ORIGIN} = require('./config');
+const {DATABASE_URL, PORT} = require('./config');
+const { Borrowd } = require('./models');
 
 app.use(
-    cors({
-
-    })
+    cors({})
 );
 
  app.get('/api/*', (req, res) => {
    res.json({ok: true});
  });
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+let server;
 
- module.exports = {app};
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { runServer, app, closeServer };
