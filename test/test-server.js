5246b9b4b65ea38bdcc9f28e22a848ecb60c7f0a
Joey Kathan <kathan.joey@gmail.com>
2018-10-15 18:01:53 -0400

    testing routes

--- test/test-server.js
+++ test/test-server.js
@@ -84,7 +84,7 @@ describe('Borrowd API resource', function () {
           res.body.should.have.lengthOf.at.least(1);
           res.body.forEach(function (post) {
             post.should.be.a('object');
-            post.should.include.keys('board', 'newId');
+            post.should.include.keys('id', 'board', 'newId');
           });
           // just check one of the posts that its values match with those in db
           // and we'll assume it's true for rest
@@ -131,7 +131,7 @@ describe('Borrowd API resource', function () {
 describe('PUT endpoint', function () {
 
     // strategy:
-    //  1. Get an existing post from dbgit 
+    //  1. Get an existing post from dbgit
     //  2. Make a PUT request to update that post
     //  4. Prove post in db is correctly updated
     it('should update fields you send over', function () {
